874be492769fde3db55eb9e37f8ed7f54002823e
francosejas <frsvt96@gmail.com>
2021-05-28 19:29:59 +0200

    ACTUA

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// seccion de codigo javascript
-function guardaUsr() {
-    // guardo la variable nomUsr con el usuario ingresado en el formulario
-    sessionStorage.setItem("nomUsr", document.forms["miFormulario"]["usuario"].value);
-    // carga la pagina de logout...
-    window.open("./html/logoutindex.html");
-    window.close();
-}
